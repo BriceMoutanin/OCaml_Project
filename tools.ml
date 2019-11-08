a15fb09023f07317a9ac60518db07653ca280826
Brice <brice.moutanin@orange.fr>
2019-11-08 10:34:49 +0100

    Fin de gmap et début de add_arc

--- tools.ml
+++ tools.ml
@@ -3,5 +3,7 @@ let clone_nodes g = n_fold g new_node empty_graph
 
 let gmap gr f =
   let gr2 = clone_nodes gr in
-  (*ligne pour convertir les arcs*)
-  e_fold gr new_arc gr2
+  e_fold gr (fun g id1 id2 a -> new_arc g id1 id2 (f a))  gr2
+
+let add_arc g id1 id2 n =
+	let g2 = clone_nodes g in
