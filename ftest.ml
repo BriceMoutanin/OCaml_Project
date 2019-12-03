8604e84fe3f996831908c04244f1079b317f1807
Brice <brice.moutanin@orange.fr>
2019-12-03 21:51:34 +0100

    Correction de ftest.ml pour ne prendre qu'un seul argument

--- ftest.ml
+++ ftest.ml
@@ -9,22 +9,27 @@ open Biparti
 let () =
 
   (* Check the number of command-line arguments *)
-  if Array.length Sys.argv <> 5 then
+  if Array.length Sys.argv <> 2 then
+    begin
+      Printf.printf "\nUsage: %s infile \n\n%!" Sys.argv.(0) ;
+      exit 0
+    end ;
+  (*if Array.length Sys.argv <> 5 then
     begin
       Printf.printf "\nUsage: %s infile source sink outfile\n\n%!" Sys.argv.(0) ;
       exit 0
-    end ;
+    end ;*)
 
 
   (* Arguments are : infile(1) source-id(2) sink-id(3) outfile(4) *)
   
-  let infile = Sys.argv.(1)
-  and outfile = Sys.argv.(4)
+  let infile = Sys.argv.(1) in
+  (* and outfile = Sys.argv.(4)
   
   (* These command-line arguments are not used for the moment. *)
   and _source = int_of_string Sys.argv.(2)
   and _sink = int_of_string Sys.argv.(3)
-  in
+  in *)
 
   (* Open file *)
   let (l1,l2) = from_bfile infile in
